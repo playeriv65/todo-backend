3ad4ffb2bcd93771ed11fa67d5e1932d177b7f21
playeriv65 <playeriv65@gmail.com>
2026-02-18 12:04:19 -0600

    add git workflow and docker

--- src/main/java/io/github/playeriv65/todobackend/HelloController.java
+++ src/main/java/io/github/playeriv65/todobackend/HelloController.java
@@ -1,4 +1,13 @@
 package io.github.playeriv65.todobackend;
 
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
 public class HelloController {
+
+    @GetMapping("/hello")
+    public String hello() {
+        return "Hello World!";
+    }
 }
