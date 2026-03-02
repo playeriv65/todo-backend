841232a41c7ca21e34ae16745291b947810f5c0c
playeriv65 <playeriv65@gmail.com>
2026-03-01 22:19:33 -0600

    feat: Basic auto test using h2.

--- src/test/java/io/github/playeriv65/todobackend/TodoBackendApplicationTests.java
+++ src/test/java/io/github/playeriv65/todobackend/TodoBackendApplicationTests.java
@@ -2,8 +2,10 @@ package io.github.playeriv65.todobackend;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class TodoBackendApplicationTests {
 
     @Test
